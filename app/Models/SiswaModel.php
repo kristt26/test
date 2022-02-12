bed14f8eea84867a4967c61e838563b57b3206fb
Ajenkris Kungkung <kristt26@gmail.com>
2022-02-12 22:41:34 +0900

    sdsd

--- app/Models/SiswaModel.php
+++ app/Models/SiswaModel.php
@@ -3,18 +3,16 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
-use mysqli;
-use phpDocumentor\Reflection\Types\This;
 
 class SiswaModel extends Model
 {
     protected $table            = 'tb_siswa';
-    protected $primaryKey       = 'id_siswa'; 
+    protected $primaryKey       = 'id_siswa';
     protected $returnType       = 'object';
-    protected $allowedFields    = ['nik','nama_siswa','jenis_kelamin','tempat_lahir','tanggal_lahir','agama','alamat',
+    protected $allowedFields    = ['nik', 'nis','nama_siswa','jenis_kelamin','tempat_lahir','tanggal_lahir','agama','alamat',
                                 'dusun','wilaya','kabupaten_kota','kecamatan','kelurahan','jenis_tinggal','transportasi',
                                 'nohp','nama_ayah','pekerjaan_ayah','nama_ibu','pekerjaan_ibu','upload_foto3x4',
-                                'upload_ijazah','upload_ktp','tahun_masuk','id_user','nis'];
+                                'upload_ijazah','upload_ktp','tahun_masuk','id_user'];
   
   
     public function getUser(){
@@ -32,36 +30,12 @@ class SiswaModel extends Model
     }
    
    public function detail($id_siswa= null){
-     if($id_siswa === null){
-       return  $this->db->query("SELECT
-       `tb_siswa`.*
-     FROM
-       `tb_siswa` ")->getResult();
-     }
      $data = $this->db->query("SELECT
      `tb_siswa`.*
    FROM
-     `tb_siswa` WHERE id_siswa = $id_siswa ORDER BY nis DESC")->getResultArray();
+     `tb_siswa` WHERE id_siswa = $id_siswa")->getResultArray();
      return $data;
    }
-
-   public function siswa($id_siswa = false)
-   {
-     # code...
-     if($id_siswa == false){
-       return $this->findAll();
-     }
-    //  return $this->where([$id_siswa => 'id_siswa'])->find();
-    return   $this->db->query("SELECT
-    `tb_siswa`.*
-  FROM
-    `tb_siswa` WHERE id_siswa = $id_siswa")->getResultArray();
-   }
-
-
-
-
-   
  
     
     
